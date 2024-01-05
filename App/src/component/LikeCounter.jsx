3f022a4b39b88fa52213511d2bb0d3fc83196d31
rajashree-guha <rajashree.625.guha@gmail.com>
2024-01-05 16:22:52 +0530

    done

---
+++ App/src/component/LikeCounter.jsx
@@ -0,0 +1,24 @@
+import React, { useReducer } from "react";
+import { initialValue, Reducer } from "./Reducer";
+
+const LikeCounter = () => {
+  const [state, dispatch] = useReducer(Reducer, initialValue);
+
+  const handleIncrement = () => {
+    dispatch({ type: "INCREMENT" });
+  };
+
+  const handleDecrement = () => {
+    dispatch({ type: "DECREMENT" });
+  };
+
+  return (
+    <div>
+      <h1>{state.count}</h1>
+      <button onClick={handleIncrement}>Like</button>
+      <button onClick={handleDecrement}>Unlike</button>
+    </div>
+  );
+};
+
+export default LikeCounter;
